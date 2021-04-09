f92e4cfe6f4bcdde7be0729fb7d036cb25109cf7
KovalevichTimofei <kovta.98@mail.ru>
2021-04-09 14:20:00 +0300

    console all main

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import connectRoutes from './routes';
 import { initDb } from './models';
 
 async function main() {
+  console.log('main');
   const app = new Koa();
-
+  console.log('const app = new Koa();');
   app.use(bodyParser());
+  console.log('app.use(bodyParser());');
   app.use(cors());
-
+  console.log('app.use(cors());');
   await initDb();
+  console.log('await initDb();');
 
   app.use(async (ctx, next) => {
     await next();
@@ -19,10 +22,11 @@ async function main() {
       ctx.throw(404, { error: 'Nonexistent route!' });
     }
   });
-
+  console.log('use routes');
   connectRoutes(app);
-
+  console.log('connectRoutes(app);');
   const port = process.env.PORT || 3000;
+  console.log('set port');
   app.listen(port);
   console.log(port);
 }
